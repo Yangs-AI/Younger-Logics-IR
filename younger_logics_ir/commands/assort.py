72d30b2bb4e840dddd08e7584b092ba3f7334a28
Jason-Young-AI <AI.Jason.Young@outlook.com>
2025-12-25 22:20:56 -0500

    Update Logging Method

--- younger_logics_ir/commands/assort.py
+++ younger_logics_ir/commands/assort.py
@@ -6,7 +6,7 @@
 # Author: Jason Young (杨郑鑫).
 # E-Mail: AI.Jason.Young@outlook.com
 # Last Modified by: Jason Young (杨郑鑫)
-# Last Modified time: 2025-12-25 21:45:31
+# Last Modified time: 2025-12-25 22:20:03
 # Copyright (c) 2024 Yangs.AI
 # 
 # This source code is licensed under the Apache License 2.0 found in the
@@ -17,7 +17,7 @@
 import click
 import pathlib
 
-from younger_logics_ir.commons.logging import logger, equip_logger
+from younger_logics_ir.commons.logging import equip_logger
 
 
 @click.group(name='assort')
